02ed337e7117e1444dfd40baf2f5e1737afa3950
StevenS125 <sts12589@gmail.com>
2017-04-25 16:48:29 -0400

    made changes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
+
+
+$(document).on("click", "#getArticles", function() {
+
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
   // For each one
-  for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < 20; i++) {
     // Display the apropos information on the page
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
+}).done(alert('Added 20 new articles!'));
 });
 
 
